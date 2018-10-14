619730259250f2bd2dc1fa4821b901199be56003
Michael Roose <mjroose@gmail.com>
2018-10-14 16:20:20 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+  end
+end
