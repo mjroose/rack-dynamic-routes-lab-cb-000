47c60f97582f0a803ff2bff595096d627a2dc37d
Michael Roose <mjroose@gmail.com>
2018-10-14 16:27:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,19 @@
 class Application
-  @@item = []
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-
+      search_term = req.path.split("/items/").last
+      item = @@items.detect {|item| item.name == search_term}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.status = 404
       resp.write "Route not found"
